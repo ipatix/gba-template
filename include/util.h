815222aac5f90fb111c56af678e67b8b714cffe0
Michael Panzlaff <michael.panzlaff@fau.de>
2021-06-11 16:43:12 +0200

    add util header

---
+++ include/util.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <gba.h>
+
+static inline void DmaCopy16(u16 *dst, const u16 *src, u32 len) {
+    register u32 r_src asm("r0") = (u32)src;
+    register u32 r_dst asm("r1") = (u32)dst;
+    register u32 r_ctl asm("r2") = (u32)(len >> 1) | DMA_ENABLE;
+    register u32 r_reg asm("r3") = (u32)0x040000d4;
+    asm volatile("stmia %3!, {%0, %1, %2}\n\tsub %3, %3, #12" :: "l" (r_src), "l" (r_dst), "l" (r_ctl), "l" (r_reg) : "memory", "cc");
+}
