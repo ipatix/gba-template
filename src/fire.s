9ce0d0d842a79a2e58aefdd7175c95226c6a701f
Michael Panzlaff <michael.panzlaff@fau.de>
2021-07-13 04:07:57 +0200

    optimize fire animation

--- src/fire.s
+++ src/fire.s
@@ -32,11 +32,14 @@ main_loop:
     umull   r0, r1, r7, r11
     adds    r7, r0, #1
     adc     r6, r1, r6
-    # create subtractor
+
+    # load 16 pixels
+    ldmia   r5!, {r0, r2, r3, r12}
+
+    # create subtractor (1)
     and     r1, r9, r6
     rsb     r1, r1, r9, lsl#7
     # subtract randomized palette index of 4 pixels
-    ldmia   r5!, {r0}
     add     r0, r0, r1
     and     r1, r9, r0, lsr#7
     rsb     r1, r1, r1, lsl#7
@@ -45,7 +48,49 @@ main_loop:
     movs    r1, r6, lsl#1
     movmi   r0, r0, ror#8
     movcs   r0, r0, ror#24
-    stmia   r4!, {r0}
+
+    # create subtractor (2)
+    and     r1, r9, r6, lsr#1
+    rsb     r1, r1, r9, lsl#7
+    # subtract randomized palette index of 4 pixels
+    add     r2, r2, r1
+    and     r1, r9, r2, lsr#7
+    rsb     r1, r1, r1, lsl#7
+    and     r2, r2, r1
+    # rotate pixels around a little bit
+    movs    r1, r6, lsl#3
+    movmi   r2, r2, ror#8
+    movcs   r2, r2, ror#24
+
+    # create subtractor (3)
+    and     r1, r9, r6, lsr#2
+    rsb     r1, r1, r9, lsl#7
+    # subtract randomized palette index of 4 pixels
+    add     r3, r3, r1
+    and     r1, r9, r3, lsr#7
+    rsb     r1, r1, r1, lsl#7
+    and     r3, r3, r1
+    # rotate pixels around a little bit
+    movs    r1, r6, lsl#9
+    movmi   r3, r3, ror#8
+    movcs   r3, r3, ror#24
+
+    # create subtractor (4)
+    and     r1, r9, r6, lsr#3
+    rsb     r1, r1, r9, lsl#7
+    # subtract randomized palette index of 4 pixels
+    add     r12, r12, r1
+    and     r1, r9, r12, lsr#7
+    rsb     r1, r1, r1, lsl#7
+    and     r12, r12, r1
+    # rotate pixels around a little bit
+    movs    r1, r6, lsl#11
+    movmi   r12, r12, ror#8
+    movcs   r12, r12, ror#24
+
+    # store 16 pixels
+    stmia   r4!, {r0, r2, r3, r12}
+
     # repeat
     cmp     r5, r8
     blt     main_loop
